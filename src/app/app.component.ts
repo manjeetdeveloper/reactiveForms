105c39e56a6a38790af5518154ffa33cf52ecda0
Manjeet Singh <98460132+manjeetdeveloper@users.noreply.github.com>
2024-11-28 03:52:51 +0530

    new upadted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import {FormGroup,FormControl, Validators} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'reactiveForms';
+  title = 'Angualr Reactive Form';
+  loginForm = new FormGroup({ // ye form ko control krega
+    user:new FormControl('',[Validators.required,Validators.email]), // ye  sirf user ko control krega and second part validaters hota hia
+    password:new FormControl('',[Validators.required,Validators.minLength(5)]),
+
+  })
+  loginUser()
+  {
+    console.warn(this.loginForm.value);
+  }
+  // yaha pe ham validatrs ka message input filed ke bad shoe kra rhe hai
+  get user()
+  {
+    return this.loginForm.get('user');
+  }
+  get password()
+  {
+    return this.loginForm.get('password');
+  }
 }
